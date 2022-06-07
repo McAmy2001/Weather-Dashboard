f36f2aa849a5f040a3ffcf004478bd814d293e51
Amy McCabe <mcca0168@umn.edu>
2022-06-07 15:07:40 -0500

    city search save and display

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const cityInputEl = document.getElementById("city");
 const chosenCityEl = document.getElementById("chosenCity");
 const currentWeatherDisplay = document.getElementById("current-weather");
 const forecastDisplayEl = document.getElementById("forecast-weather");
+const pastCityListEl = document.getElementById("past-city-list");
 
 
 var displayCurrentWeather = function(weatherArray) {
@@ -45,33 +46,33 @@ var displayForecast = function(forecastArray) {
 
   for (var i = 0; i < 5; i++) {
     console.log(forecastArray[i].temp.day);
-    var forecastBoxEl = document.createElement("ul");
-    forecastBoxEl.className = "forecast-box";
+    var forecastBoxEl = document.createElement("div");
+    forecastBoxEl.className = "forecast-display-div";
     forecastDisplayEl.appendChild(forecastBoxEl);
 
-    var futureDateDisplay = document.createElement("li");
+    var futureDateDisplay = document.createElement("p");
     var timeMilleseconds = forecastArray[i].dt * 1000;
     var dateObject = new Date(timeMilleseconds);
     var formattedDate = dateObject.toLocaleString("en-US", {weekday: "long"}) + ", " + dateObject.toLocaleString("en-US", {month: "long"}) + " " + dateObject.toLocaleString("en-US", {day: "numeric"}) + ", " + dateObject.toLocaleString("en-US", {year: "numeric"});
     futureDateDisplay.textContent = formattedDate;
     forecastDisplayEl.appendChild(futureDateDisplay);
 
-    var forecastTempEl = document.createElement("li");
+    var forecastTempEl = document.createElement("p");
     forecastTempEl.innerHTML = "Temperature: " + forecastArray[i].temp.day + "&#8457";
     forecastTempEl.className = "forecast-list-item";
     forecastDisplayEl.appendChild(forecastTempEl);
 
-    var forecastHumidityEl = document.createElement("li");
+    var forecastHumidityEl = document.createElement("p");
     forecastHumidityEl.innerHTML = "Humidity: " + forecastArray[i].humidity + "%";
     forecastHumidityEl.className = "forecast-list-item";
     forecastDisplayEl.appendChild(forecastHumidityEl);
 
-    var forecastWindSpeedEl = document.createElement("li");
+    var forecastWindSpeedEl = document.createElement("p");
     forecastWindSpeedEl.innerHTML = "Wind Speed: " + forecastArray[i].wind_speed + "mph";
     forecastWindSpeedEl.className = "forecast-list-item";
     forecastDisplayEl.appendChild(forecastWindSpeedEl);
 
-    var forecastUVIEl = document.createElement("li");
+    var forecastUVIEl = document.createElement("p");
     forecastUVIEl.innerHTML = "UV Index: " + forecastArray[i].uvi;
     forecastUVIEl.className = "forecast-list-item";
     forecastDisplayEl.appendChild(forecastUVIEl);
@@ -106,7 +107,23 @@ var weatherByCoordinates = function(latLon) {
   });
 };
 
+var saveCity = function(cityInput) {
+  console.log("went to save function");
+  const storageCities = localStorage.getItem("savedCities");
+  const currentCity = {city: cityInput};
+
+  if (storageCities === null) {
+    localStorage.setItem("savedCities", JSON.stringify([currentCity]));
+  } else {
+    var citiesArray = JSON.parse(storageCities);
+    citiesArray.push(currentCity);
+    localStorage.setItem("savedCities", JSON.stringify(citiesArray));
+  }
+};
+
+
 var weatherByCity = function(city) {
+  chosenCityEl.textContent = city;
   var apiUrl = ("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b8646725b8d223e6d4478a73d1089c53");
   
   fetch(apiUrl).then(function(response) {
@@ -114,7 +131,8 @@ var weatherByCity = function(city) {
     if (response.ok) {
       //console.log(response);
       response.json().then(function(data) {
-      //console.log(data);
+      var cityInput = data.name;
+      console.log(city);
       //var initialData = data;
       var latitude = data.coord.lat;
       var latEdit = latitude.toFixed(2);
@@ -125,6 +143,7 @@ var weatherByCity = function(city) {
       var latLon = (latEdit + "&lon=" + lonEdit);
       //console.log(latLon);
       weatherByCoordinates(latLon);
+      saveCity(cityInput);
       });
     } else {
       alert("Error: city not found, please try another.");
@@ -140,11 +159,32 @@ var cityBtnHandler = function(event) {
   //console.log("Clicked");
   var city = cityInputEl.value.trim();
   //console.log(city);
-  chosenCityEl.textContent = city;
+  
   weatherByCity(city);
   cityInputEl.value = ''; 
 }
 
-  
+var listCities = function() {
+  const storageCities = localStorage.getItem("savedCities");const listedCities = JSON.parse(storageCities);
+  console.log(listedCities);
+
+  for (var i = 0; i < listedCities.length; i++) {
+    var cityListEl = document.createElement("button");
+    cityListEl.className = "past-city-btn";
+    cityListEl.textContent = listedCities[i].city;
+    pastCityListEl.appendChild(cityListEl);
+  }
+  };
+  listCities();
+
+var pastCityBtnHandler = function(e) {
+  console.log(e.target);
+  var city = e.target.textContent;
+  console.log(city);
+  weatherByCity(city);
+};
+
 chooseCityEl.addEventListener("submit", cityBtnHandler);
+pastCityListEl.addEventListener("click", pastCityBtnHandler);
+
 //weatherByCity("Oakdale");
